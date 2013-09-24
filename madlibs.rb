40b6ceb741a7c99b03044849732a8789e2ad62b3
Cori Allen <coriallen@gmail.com>
2013-09-24 14:54:03 -0400

    Create madlibs file

---
+++ madlibs.rb
@@ -0,0 +1,23 @@
+puts "Type the kind of endangered, exotic animal Rihanna will take her next selfie with."
+pet = gets.chomp
+
+puts "Type the next madeup adjective that Gretchen Weiners will try to make happen."
+adjective = gets.chomp
+
+puts "Type an adverb your most pretentious friends would use in their status updates."
+adverb = gets.chomp
+
+puts "Type a kind of container you keep in your house."
+container = gets.chomp
+
+puts "Type an instagrammed dinner food you've seen lately."
+dinner_food = gets.chomp
+
+puts "Type the name Kimye (kanye and kim kardashian) would give their pet."
+pet_name = gets.chomp
+
+puts "Type a restaurant name that could become the newest Brooklyn gastro-foodie-hyper-local-ironic-pubstravaganza."
+restaurant = gets.chomp
+
+puts "Liz Lemon goes to Dinner"
+puts "Last Friday night, after wrapping another successful episode of TGS, Liz Lemon returned to her apartment to find an empty refrigerator. Well, it wasn't completely empty; there was a #{container} of #{dinner_food} growing mold in the back corner. Since the #{container} of #{dinner_food} was most certainly not Liz'a favorite, ham, she grabbed her #{pet} named #{pet_name} and headed for the corner deli, #{restaurant}. It may not look like much on the outside, but #{restaurant} is the #{adjective}est ham joint in New York City. The line was moving #{adverb}, so Liz decided to use #{pet_name} to cut to the front. Being a very #{adjective} #{pet}, #{pet_name} scared the pants off of everyone else in line, and Liz waltzed up to #{restaurant}'s front counter, grabbed her ham in one had, her #{pet_name} in the other, and headed home. Lemon out!" 
---
+++ string_demo.rb
@@ -0,0 +1,6 @@
+# puts "Hello world."
+# p "Hello moon."
+
+# my_name = "Cori " + "Allen"
+my_name = gets.chomp
+puts "Hi, my name is #{my_name}."
