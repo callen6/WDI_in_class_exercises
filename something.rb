91b2e14d9ffbd8da7c7f08b3cb66fb8508dd4b4d
Cori Allen <coriallen@gmail.com>
2013-09-25 11:52:32 -0400

    Update from 09-24-13 class

---
+++ something.rb
@@ -0,0 +1,37 @@
+# puts "How old are you?"
+# age = gets.chomp.to_i
+# while age < 21
+# puts "No really, how old are you?"
+# age = gets.chomp.to_i
+# end
+# puts "Come on in and have a drink."
+
+
+
+
+# puts "Want some rye? Course you do."
+# want_rye = gets.chomp
+# while want_rye == "no"
+# puts "Want some rye? Course you do."
+# want_rye = gets.chomp
+# end
+# puts "Have another drink."
+
+# number_of_times = 0
+
+# while number_of_times < 10
+# put "Do it again"
+# number_of_times += 1
+# end
+
+# 4.times do
+# puts "Hi"
+# end
+
+want_to_play = "yes"
+while want_to_play == "yes"
+	puts "Game time"
+	#Code for games goes here
+	puts "You win! Play again?"
+	want_to_play = gets.chomp
+end
